42054fd1551c9c450f77bf4315c07eaa8bb520de
jimmy durak <deeppaz@hotmail.com>
2022-12-18 16:17:19 +0300

    update

--- src/Model.js
+++ src/Model.js
@@ -1,19 +1,27 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import { Mesh } from "three";
-import { useAnimations, useGLTF } from "@react-three/drei";
+import { Mesh, Scene } from "three";
+import { useAnimations, useGLTF, Html } from "@react-three/drei";
 
 export function Model() {
   const group = useRef();
-  //   const gltf = useLoader(GLTFLoader, + "model.glb");
   const { scene, animations } = useGLTF("model.glb", true);
   const { actions, mixer } = useAnimations(animations, group);
+  const [playAnimate, setPlayAnimate] = useState(false);
+
+  const onChange = () => {
+    if (playAnimate === true) {
+      actions.Animation.play();
+    } else if (playAnimate === false) {
+      actions.Animation.stop();
+    }
+  };
 
   useEffect(() => {
+    onChange();
     scene.scale.set(1, 1, 1);
     scene.position.set(0, 0, 0);
-    actions.Animation.play();
     scene.traverse((object) => {
       if (object instanceof Mesh) {
         object.castShadow = true;
@@ -21,8 +29,8 @@ export function Model() {
         object.material.envMapIntensity = 20;
       }
     });
-  }, [scene, mixer]);
+  }, [scene, mixer, playAnimate]);
 
-  return <primitive ref={group} object={scene} dispose={null} />;
+  return <primitive ref={group} object={scene} dispose={null}  onClick={() => setPlayAnimate(playAnimate == false ? true : false)}/>;
 }
 useGLTF.preload("model.glb");
